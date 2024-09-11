58bac892d8800c8e1a0e4541638d85cf38b7dec0
Fabrizio Cannizzo <smartrics@gmail.com>
2024-09-11 22:44:12 +0100

    added debug statement

--- nyx-my-data.py
+++ nyx-my-data.py
@@ -8,16 +8,18 @@ llm = ChatOpenAI(model_name="gpt-4o-mini", api_key=config.api_key)
 client = NyxLangChain(config=config, llm=llm)
 
 files_path = [os.path.abspath(x) for x in os.listdir()]
-print(files_path)
- 
-data_name = os.getenv('DATANAME', None)
-
-data_path = "data"
-
-if not os.path.exists(data_path):
-    os.makedirs(data_path)
-
-my_subscriptions = client.get_subscribed_datasets()
 print(f"name, title, content_type, description")
-for s in my_subscriptions:
-    print(f"{s.name}, {s.title}, {s.content_type}, ")
+for f in files_path:
+     print(f"{f}, , , ")    
+
+# data_name = os.getenv('DATANAME', None)
+
+# data_path = "data"
+
+# if not os.path.exists(data_path):
+#     os.makedirs(data_path)
+
+# my_subscriptions = client.get_subscribed_datasets()
+# print(f"name, title, content_type, description")
+# for s in my_subscriptions:
+#     print(f"{s.name}, {s.title}, {s.content_type}, ")
